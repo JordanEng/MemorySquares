d0aaeeab846148754b98c78d393dce4064035eba
Jordan <jordyeng@gmail.com>
2016-04-19 08:41:15 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ros_jheng/memorysquares/ClickBoard.java
@@ -0,0 +1,13 @@
+package com.example.ros_jheng.memorysquares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClickBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_board);
+    }
+}
