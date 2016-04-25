7e42db6ef45cfd548a9c855547ae98c273543bc6
Jordan <jordyeng@gmail.com>
2016-04-25 08:45:10 -0700

    Button triggers text input and grid screen

--- app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
+++ app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
@@ -10,6 +10,7 @@ public class BoardFilled extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_filled);
     }
+
     /*
        private int size;
    private int dimension;
--- app/src/main/java/com/example/ros_jheng/memorysquares/Test.java
+++ app/src/main/java/com/example/ros_jheng/memorysquares/Test.java
@@ -1,8 +1,10 @@
 package com.example.ros_jheng.memorysquares;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.util.*;
 
@@ -21,23 +23,22 @@ public class Test extends AppCompatActivity {
         intro.add(0, "In this game, you need to remember the " +
                 "placement of the red squares and try to replicate them");
         intro.add(1, "Good Luck!");
+        assert tv != null;
         tv.setText(intro.get(countPress));
         countPress++;
+
+        if(countPress == 2){
+            EditText et = (EditText)findViewById(R.id.boardSize);
+
+        }
+
+        if(countPress >= 3){
+            Intent intent = new Intent(this, BoardFilled.class);
+            startActivity(intent);
+        }
     }
 
-
-    /*      Scanner console = new Scanner(System.in);
-
-      int size = 0;
-      System.out.println("Welcome to Memory Squares!");
-      Thread.sleep(2000);
-      System.out.println("In this game, you need to remember the plaement");
-      System.out.println("of the red squares and try to get rid of them.");
-      Thread.sleep(2000);
-      System.out.println("");
-      System.out.println("Good Luck!");
-      System.out.println("");
-      Thread.sleep(2000);
+    /*
       System.out.print("Input the size of your board (2-10): ");
       size = console.nextInt();
       System.out.println("You have " + size + " seconds. Get ready...");
