42207372e4132a385992120a0aed484e1397aacc
Jordan <jordyeng@gmail.com>
2016-05-03 08:41:38 -0700

    Trying to fix GridView

--- app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
+++ app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
@@ -1,16 +1,29 @@
 package com.example.ros_jheng.memorysquares;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class BoardFilled extends AppCompatActivity {
 
     ArrayList<Integer> actualList;
+    int counter = 1;
+    CustomGrid adapter;
+    static int column = 2;
+    public static GridView gridview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +36,141 @@ public class BoardFilled extends AppCompatActivity {
                 Intent i = new Intent(BoardFilled.this, ClickBoard.class);
                 startActivity(i);
 
-                Random rand = new Random();
-                int random = rand.nextInt(2);
-                if(random == 0){
-                    actualList.add(1);
-                } else{
-                    actualList.add(0);
-                }
-                Integer[] intArray = actualList.toArray(new Integer[0]);
+                adapter = new CustomGrid(MainActivity.this);
+                gridview = (GridView) findViewById(R.id.gridview);
 
+                gridview.setNumColumns(2);
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<Integer>(this,
-                        android.R.layout.simple_list_item_1, stringArray);
+                gridview.setAdapter(adapter);
 
-                finish();
             }
         }, 10000);
     }
+
+    public class CustomGrid extends BaseAdapter {
+        private Context mContext;
+        CustomGrid adgg;
+        int mNumColumns = 2;
+        private Random rand = new Random();
+        GridView gg;
+        int color = Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        float alfa = 0.1f;
+        int rand_ind = rand.nextInt((2 * 2) - 0) + 0;
+
+        public CustomGrid(Context c) {
+            mContext = c;
+            // this.Imageid = Imageid;
+            // this.web = web;
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return mNumColumns * mNumColumns;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+
+            View gridView;
+
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View grid = new View(mContext);
+            grid = inflater.inflate(R.layout.activity_board_filled, null);
+            final View imageView = (ImageView) grid.findViewById(R.id.image_grid);
+
+            if (convertView == null) {
+                if (position == rand_ind) {
+                    imageView.setBackgroundColor(getColorWithAlpha(color, alfa));
+                } else {
+                    imageView.setBackgroundColor(color);
+                }
+            } else {
+                grid = (View) convertView;
+            }
+
+            grid.setOnClickListener(new View.OnClickListener() {
+
+
+                public void onClick(View v) {
+
+
+                    if (position == rand_ind) {
+
+                        // Correct postion click time if condition occur..........
+
+
+                        if (counter == 2) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.1f;
+                        } else if (counter == 6) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.08f;
+                        } else if (counter == 8) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.1f;
+                        } else if (counter == 12) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.1f;
+                        } else if (counter == 14) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.1f;
+                        } else if (counter == 16) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.1f;
+                        } else if (counter == 18) {
+                            mNumColumns = mNumColumns + 1;
+                            alfa = alfa + 0.05f;
+                        } else if (counter >= 18) {
+                            // mNumColumns = mNumColumns + 1;
+                            alfa = 0.8f;
+
+                        }
+
+                        gridview.setNumColumns(mNumColumns);
+                        gridview.setAdapter(adapter);
+                        adapter.notifyDataSetChanged();
+
+                        rand_ind = rand.nextInt((mNumColumns * mNumColumns) - 0) + 0;
+
+                    } else {
+
+                        // Here code for nagative click............ whem click
+                        // nagative at time geanarate this code.........
+                    }
+                }
+            });
+
+            return grid;
+        }
+
+        public int getColorWithAlpha(int color, float ratio) {
+            int newColor = 0;
+            int alpha = Math.round(Color.alpha(color) * ratio);
+            int r = Color.red(color);
+            int g = Color.green(color);
+            int b = Color.blue(color);
+            newColor = Color.argb(alpha, r, g, b);
+            return newColor;
+        }
+    }
+
+    public void onClick(View v) {
+    }
+}
 
 
     /*
