b05f56e4c936d0f85277ae5bb0e205981891a448
Jordan <jordyeng@gmail.com>
2016-05-16 08:39:02 -0700

    Eh. Less broken, adds button through class

--- app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
+++ app/src/main/java/com/example/ros_jheng/memorysquares/BoardFilled.java
@@ -1,13 +1,13 @@
 package com.example.ros_jheng.memorysquares;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.RadioGroup;
+
 import java.util.ArrayList;
 
 public class BoardFilled extends AppCompatActivity {
@@ -18,32 +18,55 @@ public class BoardFilled extends AppCompatActivity {
         setContentView(R.layout.activity_board_filled);
         setTitle("Watch the Squares");
 
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.boardfilled);
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.image_grid);
+        assert gridLayout != null;
+        gridLayout.setUseDefaultMargins(true);
+        gridLayout.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
 
         int total = 4;
         int column = 2;
         int row = total / column;
-        assert gridLayout != null;
         gridLayout.setColumnCount(column);
-        gridLayout.setRowCount(row + 1);
-        for (int i = 0, c = 0, r = 0; i < total; i++, c++) {
-            if (c == column) {
-                c = 0;
-                r++;
-            }
-            Button buttonGrid = new Button(this);
-            buttonGrid.setLayoutParams(new RadioGroup.LayoutParams(100, 100));
+        gridLayout.setRowCount(row + column);
+        int currentRow = 0;
+        int currentColumn = 0;
 
-            GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
-            GridLayout.Spec colspan = GridLayout.spec(GridLayout.UNDEFINED, 1);
-            if (r == 0 && c == 0) {
-                //Log.e("", "spec");
-                colspan = GridLayout.spec(GridLayout.UNDEFINED, 2);
-                rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 2);
+        for (int i = 0; i < total; i++) {
+            if (currentColumn == column ) {
+                currentColumn = 0;
+                currentRow++;
             }
-            GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(rowSpan, colspan);
-            gridLayout.addView(buttonGrid, gridParam);
+                Button button = new Button(this);
+                button.setBackgroundColor(Color.RED);
+                button.setHeight(1000);
+                button.setWidth(100);
+
+                GridLayout.Spec rowspan = GridLayout.spec(currentRow, 1);
+                GridLayout.Spec colspan = GridLayout.spec(currentColumn, 1);
+                GridLayout.LayoutParams gridLayoutParam = new GridLayout.LayoutParams(rowspan, colspan);
+                gridLayout.addView(button,gridLayoutParam);
+                currentColumn++;
+
         }
+
+//        for (int i = 0, c = 0, r = 0; i < total; i++, c++) {
+//            if (c == column) {
+//                c = 0;
+//                r++;
+//            }
+//            Button buttonGrid = new Button(this);
+//            buttonGrid.setLayoutParams(new RadioGroup.LayoutParams(100, 100));
+//
+//            GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
+//            GridLayout.Spec colspan = GridLayout.spec(GridLayout.UNDEFINED, 1);
+//            if (r == 0 && c == 0) {
+//                Log.e("", "spec");
+//                colspan = GridLayout.spec(GridLayout.UNDEFINED, 2);
+//                rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 2);
+//            }
+//            GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(rowSpan, colspan);
+//            gridLayout.addView(buttonGrid, gridParam);
+//        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -57,153 +80,6 @@ public class BoardFilled extends AppCompatActivity {
     }
 }
 
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-//
-//        GridView gridview = (GridView) findViewById(R.id.gridview);
-//        gridview.setAdapter(new CustomButtonListener(this));
-//
-//        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            public void onItemClick(AdapterView<?> parent, View v,
-//                                    int position, long id) {
-//                Toast.makeText(BoardFilled.this, "" + position,
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-
-
-//    public class CustomGrid extends BaseAdapter {
-//        private Context mContext;
-//        CustomGrid adgg;
-//        int mNumColumns = 2;
-//        private Random rand = new Random();
-//        GridView gg;
-//        int color = Color.argb(255, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
-//        float alfa = 0.1f;
-//        int rand_ind = rand.nextInt((2 * 2) - 0) + 0;
-//
-//        public CustomGrid(Context c) {
-//            mContext = c;
-//            // this.Imageid = Imageid;
-//            // this.web = web;
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            // TODO Auto-generated method stub
-//            return mNumColumns * mNumColumns;
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            // TODO Auto-generated method stub
-//            return null;
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            // TODO Auto-generated method stub
-//            return 0;
-//        }
-//
-//        @Override
-//        public View getView(final int position, View convertView, ViewGroup parent) {
-//            // TODO Auto-generated method stub
-//
-//            View gridView;
-//
-//            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            View grid = new View(mContext);
-//            grid = inflater.inflate(R.layout.activity_board_filled, null);
-//            final View imageView = (ImageView) grid.findViewById(R.id.image_grid);
-//
-//            if (convertView == null) {
-//                if (position == rand_ind) {
-//                    imageView.setBackgroundColor(getColorWithAlpha(color, alfa));
-//                } else {
-//                    imageView.setBackgroundColor(color);
-//                }
-//            } else {
-//                grid = (View) convertView;
-//            }
-//
-//            grid.setOnClickListener(new View.OnClickListener() {
-//
-//
-//                public void onClick(View v) {
-//
-//
-//                    if (position == rand_ind) {
-//
-//                        // Correct postion click time if condition occur..........
-//
-//
-//                        if (counter == 2) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.1f;
-//                        } else if (counter == 6) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.08f;
-//                        } else if (counter == 8) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.1f;
-//                        } else if (counter == 12) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.1f;
-//                        } else if (counter == 14) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.1f;
-//                        } else if (counter == 16) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.1f;
-//                        } else if (counter == 18) {
-//                            mNumColumns = mNumColumns + 1;
-//                            alfa = alfa + 0.05f;
-//                        } else if (counter >= 18) {
-//                            // mNumColumns = mNumColumns + 1;
-//                            alfa = 0.8f;
-//
-//                        }
-//
-//                        gridview.setNumColumns(mNumColumns);
-//                        gridview.setAdapter(adapter);
-//                        adapter.notifyDataSetChanged();
-//
-//                        rand_ind = rand.nextInt((mNumColumns * mNumColumns) - 0) + 0;
-//
-//                    } else {
-//
-//                        // Here code for nagative click............ whem click
-//                        // nagative at time geanarate this code.........
-//                    }
-//                }
-//            });
-//
-//            return grid;
-//        }
-//
-//        public int getColorWithAlpha(int color, float ratio) {
-//            int newColor = 0;
-//            int alpha = Math.round(Color.alpha(color) * ratio);
-//            int r = Color.red(color);
-//            int g = Color.green(color);
-//            int b = Color.blue(color);
-//            newColor = Color.argb(alpha, r, g, b);
-//            return newColor;
-//        }
-//    }
-//
-//    public void onClick(View v) {
-//    }
-
-//       private int size;
-//   private int dimension;
-//   private ArrayList<Integer> redSquares;
-//
-//   // Constructor for the filled board
-//
 //
 //   // Generates the board with randomly colored squares by
 //   // by taking the number the user submitted before.
